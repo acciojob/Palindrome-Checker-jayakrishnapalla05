4890de0cfb8c2a32b2a420acf791d11eefb2fd48
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-27 14:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
+	let edited=str.replaceAll(/[^A-Za-z0-9]/g, "");
+	let start=0;
+	let end=edited.length-1;
+	while(start<=end)
+		{
+			if(edited[start] !=edited[end])
+			{
+				return false;
+			}
+			start++;
+			end--;
+		}
+	return true;
+	
 
 }
 module.exports = palindrome
